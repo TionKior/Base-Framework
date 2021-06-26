8d19db870d4b1d1399dbe7cee9ce4c6a29b74dfb
TionKior <1423304936@qq.com>
2021-06-26 10:33:15 +0800

    feat(MapperScan): SpringBoot基本配置

--- src/main/java/com/tionkior/BaseFrameworkApplication.java
+++ src/main/java/com/tionkior/BaseFrameworkApplication.java
@@ -1,5 +1,6 @@
 package com.tionkior;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
+@MapperScan(basePackages = {"com.tionkior.mapper"})
 public class BaseFrameworkApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/tionkior/domain/User.java
+++ src/main/java/com/tionkior/domain/User.java
@@ -16,9 +16,9 @@ import lombok.NoArgsConstructor;
 @Data //提供各种方法
 @NoArgsConstructor // 提供无参构造方法
 @AllArgsConstructor // 提供带参构造方法
-@TableName("user") // 数据库表名
+@TableName("user") // 数据库表名映射
 public class User {
-//    @TableId(type = IdType.AUTO) // 数据库主键,自动增长,已经设置了全局id生成策略
+
     private Long id;
 
     private String username;
